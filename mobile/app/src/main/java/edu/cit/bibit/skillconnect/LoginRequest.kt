c3a1e92f048bf67d9d734a7d543be477b2ad463d
Paul Bibit <paulbibit05@gmail.com>
2026-04-07 00:15:52 +0800

    Applied Singleton Pattern to RetrofitClient and DTO Pattern to LoginRequest

--- mobile/app/src/main/java/edu/cit/bibit/skillconnect/LoginRequest.kt
+++ mobile/app/src/main/java/edu/cit/bibit/skillconnect/LoginRequest.kt
@@ -3,4 +3,8 @@ package edu.cit.bibit.skillconnect
 data class LoginRequest(
     val email: String,
     val password: String
-)
+) {
+    fun isValid(): Boolean {
+        return email.isNotBlank() && password.isNotBlank()
+    }
+}
